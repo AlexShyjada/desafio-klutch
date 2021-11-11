545339ef65a9e6cd110181d057b56d71c15ee33d
Alexandre Shyjada <alexandreshyjada@infojr.com.br>
2021-11-10 23:22:21 -0300

    Update: Hero com props e pagina index com render condicional

--- project/src/pages/index.tsx
+++ project/src/pages/index.tsx
@@ -1,19 +1,28 @@
 import type { NextPage } from 'next'
+import {useState} from 'react'
 import Head from 'next/head'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
 import InputSimulacao from '../components/InputSimulacao'
+import InputClient from '../components/InputClient'
 import Table from '../components/Table'
 
 const Home: NextPage = () => {
+  const [formStep, setFormStep] = useState(1)
   return (
     <>
       <Head>
         <title>Desafio Klutch</title>
       </Head>
-      <Hero/>
-      <InputSimulacao/>
-      <Table/>
+
+      {formStep == 1 ? <Hero title="Simulação de Taxas" imgURL="/assets/img_SimulacaoDeTaxas.svg" imgWidth={225} imgHeight={115}/> : ""}
+      {formStep == 1 ? <InputSimulacao/> : ""}
+      {formStep == 1 ? <Table/> : ""}
+
+      {formStep == 2 ? <Hero title="Simulação de Taxas" imgURL="/assets/img_SimulacaoDeTaxas.svg" imgWidth={225} imgHeight={115}/> : ""}
+      {formStep == 2 ? <InputClient/> : ""}
+      {formStep == 2 ? <Table/> : ""}
+
       <Footer/>
     </>
   )
