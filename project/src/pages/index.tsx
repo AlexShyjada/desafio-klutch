24cc961b9d4700d2d0f175840ed2edfd0335c1a3
Alexandre Shyjada <alexandreshyjada@infojr.com.br>
2021-11-10 22:01:39 -0300

    Add: Input Valor Desejado

--- project/src/pages/index.tsx
+++ project/src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
+import InputSimulacao from '../components/InputSimulacao'
 import Table from '../components/Table'
 
 const Home: NextPage = () => {
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
         <title>Desafio Klutch</title>
       </Head>
       <Hero/>
+      <InputSimulacao/>
       <Table/>
       <Footer/>
     </>
